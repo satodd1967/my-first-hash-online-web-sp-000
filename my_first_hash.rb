e124cfcfd19003ce8d95dc6aaeb7e77c5c042888
Seth Todd <seth.todd@outlook.com>
2020-02-07 02:28:17 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -3,8 +3,10 @@ def my_hash
 end
 
 def shipping_manifest
-  the_manifest = {
-
+  the_manifest = {"whale bone corsets" => 5, 
+                  "porcelain vases" => 2, 
+                  "oil paintings" => 3
+  }
 end
 
 def retrieval
